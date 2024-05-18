907c3f3315979b65eff7b860960fefd9f3ddacf5
Carlos Ojeda <cojeda@navisaf.com>
2024-05-18 07:31:57 -0500

    Archivo Calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasUnitarias
+{
+    public class Calculadora
+    {
+
+        public int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Resta(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicacion(int a, int b)
+        {
+            return a * b;
+        }
+
+        public float Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("No puedes dividir entre 0.");
+            }
+            return (float)a / b;
+        }
+    }
+}
